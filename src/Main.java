c20b7d05d7678c855641f59d34b0e9d45eaecbb5
nkatakura <75689368+nkatakura@users.noreply.github.com>
2021-01-25 17:49:01 -0600

    gui edits

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
 package src;
 
-import javax.swing.*;
-
 public class Main {
-
-    public Main(){
-        String title = "MeezerMap by Naoki";
-        new MyFrame(title);
-        JLabel label = new JLabel();
-        label.setText(" ");
-        
-    }
     public static void main (String [] args){
-        new Main();
+        String title = "MeezerMap by Naoki";
+        new Window(title);
         int[][] coords = DataReader.coordArrayMaker("coords.csv");
         String[] names = DataReader.fileArrayMaker("names.csv");
     }
