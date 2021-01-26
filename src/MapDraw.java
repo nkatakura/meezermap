d9a928402b61feee71e18393b176df9cb2a24de5
nkatakura <75689368+nkatakura@users.noreply.github.com>
2021-01-26 14:23:20 -0600

    gui updates
    integrated the coordinate inputs into the main menu
    changed to make the draw function open a new window with the map drawn

---
+++ src/MapDraw.java
@@ -0,0 +1,5 @@
+package src;
+
+public class MapDraw {
+    //constructors that take each of the following: int[], int[][], and (int[][], string[])
+}
--- src/Window.java
+++ src/Window.java
@@ -12,10 +12,15 @@ public class Window extends JFrame implements ActionListener {
     JButton searchButton;
     JButton showAllButton;
     JButton clearButton;
-    MapDrawArea drawArea;
     int x, z;
     Window() {
-        drawArea = new MapDrawArea();
+        JTextField xInput, zInput;
+        JLabel xLabel = new JLabel();
+        JLabel zLabel = new JLabel();
+        xLabel.setText("x coordinate:");
+        zLabel.setText("z coordinate:");
+        xInput = new JTextField(6);
+        zInput = new JTextField(6);
         String title = "MeezerMap by Naoki";
         JPanel buttonPanel = new JPanel();
         buttonPanel.setBackground(Color.LIGHT_GRAY);
@@ -24,6 +29,10 @@ public class Window extends JFrame implements ActionListener {
         enterCoordsButton = new JButton();
         enterCoordsButton.setText("Enter Coordinates to show");
         enterCoordsButton.addActionListener(this);
+        buttonPanel.add(xLabel);
+        buttonPanel.add(xInput);
+        buttonPanel.add(zLabel);
+        buttonPanel.add(zInput);
         buttonPanel.add(enterCoordsButton);
 
         searchButton = new JButton();
@@ -45,42 +54,14 @@ public class Window extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
         this.setVisible(true);
-        this.add(drawArea);
         this.add(buttonPanel);
         this.setResizable(false);
         this.pack();
     }
 
-    public void coordinateInput() {
-        JButton enter;
-        JTextField xInput, zInput;
-        int x, z;
-        JFrame coordInput = new JFrame();
-        JLabel xLabel = new JLabel();
-        JLabel zLabel = new JLabel();
-        xLabel.setText("x coordinate:");
-        zLabel.setText("z coordinate:");
-        xInput = new JTextField(6);
-        zInput = new JTextField(6);
-        enter = new JButton();
-        enter.setText("Enter");
-        enter.addActionListener(this);
-        coordInput.setSize(200, 100);
-        coordInput.setLayout(new FlowLayout());
-        coordInput.setVisible(true);
-        coordInput.add(xLabel);
-        coordInput.add(xInput);
-        coordInput.add(zLabel);
-        coordInput.add(zInput);
-        coordInput.add(enter);
-        coordInput.setResizable(false);
-        coordInput.pack();
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == enterCoordsButton) {
-            coordinateInput();
             choice = 1;
         }
         if (e.getSource() == searchButton) {
