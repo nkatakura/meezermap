d0cf85a91e1721a6bf84f80f7e9636b5f1a26436
nkatakura <75689368+nkatakura@users.noreply.github.com>
2021-01-26 10:21:18 -0600

    more GUI updates

--- src/CoordinateInput.java
+++ src/CoordinateInput.java
@@ -7,22 +7,32 @@ import javax.swing.*;
 public class CoordinateInput extends JFrame implements ActionListener {
 
     JButton enter;
+    JTextField xInput, yInput;
 
     CoordinateInput() {
+        xInput = new JTextField(6);
+        yInput = new JTextField(6);
         enter.setText("Enter");
         this.setSize(200, 100);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         this.setSize(750, 500);
         this.setVisible(true);
+        this.add(xInput);
+        this.add(yInput);
         this.add(enter);
         this.setResizable(false);
+    }
+
+    public int intParser(String input) {
+        if (input.charAt(0) == '-')
+        return x;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == enter) {
-            
+
         }
     }
 
